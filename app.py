b0c106801a8845b7a9218b432ec51442bebe6892
aahadley <Aaron.Hadley@knights.ucf.edu>
2019-03-02 04:24:27 -0500

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/info")
+def get_user_info():
+    return {"" : ""}
+
+# Get price range from user
+@app.route("/range")
+def set_price_range():
+    return (10, 20)
+
+# Search for items within a set price range
+@app.route("/items")
+def select_items(price_range):
+    return None
+
+# Build cart from selected items
+@app.route("/cart")
+def build_cart(items):
+    return None
+
+# Finalize purchase with user information
+@app.route("/finalize")
+def commit_purchase():
+    return None
+
+if __name__ == "__main__":
+    app.run()
