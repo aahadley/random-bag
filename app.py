bfd0b7f56e07ec6f7603fcb5dc37db3f0544b86f
aahadley <Aaron.Hadley@knights.ucf.edu>
2019-03-02 15:35:39 -0500

    Added qrng function

--- app.py
+++ app.py
@@ -1,6 +1,53 @@
 from flask import Flask, render_template
 import requests
 
+import numpy as np
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import IBMQ, execute, BasicAer as aer
+from qiskit.tools.monitor import job_monitor
+from qiskit.providers.ibmq import least_busy
+
+def qrng(phys_or_sim, size=16, qubits=4, max_credits=3):
+    '''Return a random size bit integer'''
+
+    if phys_or_sim in ['p', "phys", "physical"]:
+        IBMQ.load_accounts()
+        large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits >= 4 and not x.configuration().simulator)
+        backend = least_busy(large_enough_devices)
+        #print("Selected", backend.name())
+
+    elif phys_or_sim in ['s', "sim", "simulated"]:
+        backend = aer.get_backend('qasm_simulator')
+
+    shots = size//qubits
+
+    # quantum register with 8 qubits
+    qreg = QuantumRegister(qubits, 'q')
+    creg = ClassicalRegister(qubits, 'c')
+
+    print("created registers")
+
+    # quantum circuit acting on q
+    circ = QuantumCircuit(qreg, creg)
+
+    for qubit in qreg:
+        circ.h(qubit)
+
+    for i in range(qubits):
+        circ.measure(qreg[i], creg[i])
+
+    job = execute(circ, backend, shots=shots, max_credits=max_credits)
+    #job_monitor(job)
+    result = job.result()
+    counts = result.get_counts(circ)
+    #print(counts)
+    num = ""
+
+    for i in list((counts.keys())):
+        num += i
+
+    return int(num, 2)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -32,4 +79,5 @@ def commit_purchase():
     return None
 
 if __name__ == "__main__":
+    print(qrng("sim", size=32))
     app.run()
