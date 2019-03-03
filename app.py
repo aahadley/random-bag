46f112c541b115cb58d78a0947058f1324c5def8
aahadley <Aaron.Hadley@knights.ucf.edu>
2019-03-02 21:59:58 -0500

    prepare merge

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-import requests, jsonify
+import requests, json
 
 import numpy as np
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import IBMQ, execute, BasicAer as aer
 from qiskit.tools.monitor import job_monitor
 from qiskit.providers.ibmq import least_busy
+
+###
+zinctoken = "AA35D74A1E351A42883BCA7C"
+###
 
 def qrng(phys_or_sim, size=16, qubits=4, max_credits=3):
     '''Return a random size bit integer'''
@@ -25,7 +29,7 @@ def qrng(phys_or_sim, size=16, qubits=4, max_credits=3):
     qreg = QuantumRegister(qubits, 'q')
     creg = ClassicalRegister(qubits, 'c')
 
-    print("created registers")
+    #print("created registers")
 
     # quantum circuit acting on q
     circ = QuantumCircuit(qreg, creg)
@@ -56,7 +60,9 @@ def main():
 
 @app.route("/info")
 def get_user_info():
-    return {"" : ""}
+    f = open("default_user.json", "r")
+    user = json.load(f)
+    return user
 
 # Get price range from user
 @app.route("/range")
@@ -70,14 +76,26 @@ def select_items(price_range):
 
 # Build cart from selected items
 @app.route("/order")
-def build_order(items):
-    '''takes a list of product objects'''
-    return None
+def build_order():
+    '''takes a list of product objects. No idea if this is even close to right'''
 
-# Finalize purchase with user information
-@app.route("/finalize")
-def commit_purchase():
-    return None
+    user = get_user_info()
+    price_range = set_price_range()
+
+    # Build a list of product-id pairs
+    products = []
+    
+    for p in select_items(price_range):
+        products.append({"product_id": p["product_id"] , "quantity": 1})
+
+    user.pop("products", None)
+
+    # add products into user
+    user["products"] = products
+
+    req = requests.post("https://api.zinc.io/v1/orders", auth=(zinctoken, ''), data=json.dumps(user) ).text
+    print(req) # for debugging
+    return req
 
 if __name__ == "__main__":
     #print(qrng("sim", size=32))
