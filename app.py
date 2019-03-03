7fd37b8eba91066deeb07bdfef070aff9c1288ba
Briandblalock <Briandblalock@gmail.com>
2019-03-02 21:54:33 -0500

    product generator

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 import requests, jsonify
-
+import random
+import json
+from requests.auth import HTTPBasicAuth
 import numpy as np
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import IBMQ, execute, BasicAer as aer
 from qiskit.tools.monitor import job_monitor
 from qiskit.providers.ibmq import least_busy
 
+zinctoken =  "AA35D74A1E351A42883BCA7C";
 def qrng(phys_or_sim, size=16, qubits=4, max_credits=3):
     '''Return a random size bit integer'''
 
@@ -61,16 +64,59 @@ def get_user_info():
 # Get price range from user
 @app.route("/range")
 def set_price_range():
-    return (10, 20)
+    return (10, 500, 200)
 
 # Search for items within a set price range
 @app.route("/items")
-def select_items(price_range):
+def select_items(price_range,qnum):
+    crng= random.SystemRandom()
+    products = []
+    for i in range (3):
+
+        if(i == 0):
+            xornum = 0;
+        else:
+            xornum = crng.randint(1, qnum)
+        itemnum = qnum ^ xornum
+
+        url = "https://api.zinc.io/v1/search?query={}&page=1&retailer=amazon".format(itemnum)
+        print(url)
+
+        res = requests.get(url, auth=(zinctoken, '')).json()
+
+        products += res['results']
+
+    spent = 0
+    full = False
+    priced = []
+
+    bought = []
+    for i in products:
+        if( "price" in i):
+            priced.append(i)
+
+    i = 0
+    while len(priced) > 0:
+
+        print (len(priced))
+
+        if( price_range[0] > price_range[1] - spent ):
+            break;
+
+        ch = crng.choice(priced)
 
 
+        if(ch['price']/100 > price_range[1] - spent or ch['price']/100 > price_range[2]):
+            priced.remove(ch)
+            continue
 
-    return None
+        if( ch['price']/100 > price_range[0] and ch['price']/100 < price_range[2] ):
+            bought.append(ch)
 
+            spent += int(ch['price']/100)
+
+        priced.remove(ch)
+    return (bought , spent)
 # Build cart from selected items
 @app.route("/order")
 def build_order(items):
@@ -83,5 +129,14 @@ def commit_purchase():
     return None
 
 if __name__ == "__main__":
-    #print(qrng("sim", size=32))
+    #IBMQ.save_account("8f3a83f3bdc66434c7c68f2f6f00eac509445fc9c46414128ddcea6ab31d99d505e0726a32733ce631d52d121d7b2ef0919f278d3102f822147efcd8a2f5ff3d")
+    bought , spent = select_items(set_price_range(), qrng("s"))
+
+    for i in bought:
+        print (i['title'] , " : " , i['price']/100.0)
+
+    print("spent : " , spent)
+
+
+
     app.run()
